67284ffe6b61eb9a07159b5cb3dbfcddb25a6c25
F-ryosuke-39 <r.fujita.sys19@morijyobi.ac.jp>
2020-06-23 10:43:32 +0900

    second

---
+++ INSERT.php
@@ -0,0 +1,27 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'nroot';
+$password = 'nmorijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    $sql = "INSERT INTO user VALUES (id, name, age)";
+    $stmt = $dsn->prepare($sql);
+    $params = array('id' => $id, 'name' => $name, 'age' => $age);
+
+    $result = $dbh->query($sql);
+
+    header('location: index.php?flg-1');
+
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('location: index.php?flg-1->getMessage()');
+    exit();
+}
+?>
