895504dad6315484f2ec767ea6a3fa1d0137f771
F-ryosuke-39 <r.fujita.sys19@morijyobi.ac.jp>
2020-06-25 11:33:27 +0900

    b

---
+++ DELETE.php
@@ -0,0 +1,28 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'nroot';
+$password = 'nmorijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    $id = $_SET["id"];
+
+    $sql = "DELETE user where id = id";
+    $stmt = $dbh->prepare($sql);
+    $params = array('id' => $id);
+    $stmt ->execute($params);
+
+    header('location: index.php?flg-1');
+
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('location: index.php?flg-0' . $e->getMessage());
+    exit();
+}
+?>
--- index.php
+++ index.php
@@ -155,7 +155,7 @@ try {
                         <td><?php echo "$value[age]" ?></td>
                         <td>
                           <form action="./DELETE.php" method="$_GET">
-                            <input type="text" class="d_none" name="id" value="<?php echo "$value[id]"?>">
+                            <input type="text" class="d_none" name="id" value="<?php echo "$value[id]";?>">
                             <button type="submit" class="btn btn-denger"></button>
                           </form>
                         </td>
