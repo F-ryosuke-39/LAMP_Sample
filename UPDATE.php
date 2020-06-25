a470c0f96c5cbe7b2cf25d900f539169103476fb
F-ryosuke-39 <r.fujita.sys19@morijyobi.ac.jp>
2020-06-25 11:19:29 +0900

    4

---
+++ UPDATE.php
@@ -0,0 +1,26 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'nroot';
+$password = 'nmorijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+
+    $sql = "UPDATE user SET name = name, age = age, where id = id";
+    $stmt = $dbh->prepare($sql);
+    $params = array('id' => $id, 'name' => $name, 'age' => $age);
+    $stmt ->execute($params);
+
+    header('location: index.php?flg-1');
+
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('location: index.php?flg-0' . $e->getMessage());
+    exit();
+}
+?>
